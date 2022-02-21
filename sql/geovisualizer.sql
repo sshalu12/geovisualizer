c3e8078a61534b4a2d5d8036360852bd018ff2b9
Ashutosh Gupta <37325252+ashutoshgt@users.noreply.github.com>
2022-02-21 15:41:55 +0530

    [GG-2481] Updates poi schema and adds data for poi and state (#1)
    * [GG-2481] Updates poi schema and adds data for poi and state
    
    * [GG-2481] Updates id column type to serial primary key

--- sql/geovisualizer.sql
+++ sql/geovisualizer.sql
@@ -2,14 +2,24 @@ CREATE EXTENSION postgis;
 
 CREATE TABLE IF NOT EXISTS poi
 (
-    id SERIAL NOT NULL,
+    id SERIAL PRIMARY KEY,
     business_name CHARACTER VARYING(64) DEFAULT NULL::CHARACTER VARYING,
     address CHARACTER VARYING(165) DEFAULT NULL::CHARACTER VARYING,
-    latitude DOUBLE PRECISION,
-    longitude DOUBLE PRECISION,
-    brand_name CHARACTER VARYING(164) DEFAULT NULL::CHARACTER VARYING,
-    brand_id INTEGER,
+    city character VARYING(45) DEFAULT NULL::character VARYING,
+    state character VARYING(30) DEFAULT NULL::character VARYING,
+    zip character VARYING(15) DEFAULT NULL::character VARYING,
+    latitude double precision,
+    longitude double precision,
+    country character VARYING(2) DEFAULT 'US'::character VARYING NOT NULL,
     category_name CHARACTER VARYING(164) DEFAULT NULL::CHARACTER VARYING,
-    category_id INTEGER,
-    polygons public.geometry
+    category_id INTEGER
+);
+
+
+CREATE TABLE IF NOT EXISTS states
+(
+    id SERIAL PRIMARY KEY,
+    state CHARACTER VARYING(64) DEFAULT NULL::CHARACTER VARYING,
+    country CHARACTER VARYING(165) DEFAULT NULL::CHARACTER VARYING,
+    boundary public.geometry(Geometry,4326)
 );
