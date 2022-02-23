e734a8f2bab955778487d69148ffaf4b2fd3624a
Shalini <shalinisharma1297@gmail.com>
2022-02-23 18:48:56 +0530

    All changes have been made

--- script/state.py
+++ script/state.py
@@ -1,45 +1,41 @@
 import json
 import psycopg2
-hostname = 'postgres'
-database='geovisualizer'
-username='postgres'
-pwd=''
-port_id=5432
-conn= psycopg2.connect(
-        host=hostname,
-        dbname=database,
-        user=username,
-        password=pwd,
-        port=port_id
-    )
-cur=conn.cursor()
 
-cur.execute('SELECT * from states')
-query=cur.fetchall()
-print(query)
-cur.execute('SELECT ST_AsGeoJSON(boundary,4326)::JSONB from states ')
-q=(cur.fetchall())
-#append q data in list because q fetched data in list type and element of list is tuple 
-state=[]
-for i in range(0,len(q)):
-    state.append(q[i][0])
-k=[]
-for i in range(0,len(query)):   
-    m={
-        "type":"Feature",
-         "properties": {
-                    "id":query[i][0],
-                    "state":query[i][1],
-                    "country":query[i][2]
-                },
+hostname = "postgres"
+database = "geovisualizer"
+username = "postgres"
 
-       "geometry":state[i]
+port_id = 5432
+conn = psycopg2.connect(host=hostname, dbname=database, user=username, port=port_id)
+cur = conn.cursor()
+
+cur.execute("SELECT * from states")
+query = cur.fetchall()
+
+cur.execute("SELECT ST_AsGeoJSON(boundary,4326)::JSONB from states ")
+multiploygon = cur.fetchall()
+
+geometry = []
+state = []
+
+for i in range(0, len(query)):
+    geometry.append(multiploygon[i][0])
+
+
+for i in range(0, len(query)):
+    feature = {
+        "type": "Feature",
+        "properties": {"id": query[i][0], "state": query[i][1], "country": query[i][2]},
+        "geometry": geometry[i],
     }
-    k.append(m)
-geo=json.dumps({
-    "type": "FeatureCollection",
-         "features": k})
+    state.append(feature)
 
-with open('./usr/script/state.geojson', 'w') as f:
-    f.write(geo) 
+geo = json.dumps({"type": "FeatureCollection", "features": state})
+
+try:
+    with open("./usr/script/state.geojson", "w") as f:
+        f.write(geo)
+except EOFError as e:
+    print(e)
+
 conn.close()
