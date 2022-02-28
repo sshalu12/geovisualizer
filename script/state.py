647d5e1aa44e26cf14fd20b5f3811f8c1a2b05b5
Shalini <57828137+sshalu12@users.noreply.github.com>
2022-02-28 13:50:08 +0530

    [GG-2483]fetch state data create geojson (#3)
    * [GG-2483]fetch state data create geojson
    
    * All changes have been made
    
    * changes have been made
    
    * Update state.py
    
    * Update state.py
    
    * [GG-2482] change makefile to parametrize the command for our python service
    
    * [GG-2483] resolved conflict
    
    * [GG-2482] variable name changed

---
+++ script/state.py
@@ -0,0 +1,34 @@
+import json
+import psycopg2
+
+hostname = "postgres"
+database = "geovisualizer"
+username = "postgres"
+
+port_id = 5432
+conn = psycopg2.connect(host=hostname, dbname=database, user=username, port=port_id)
+cur = conn.cursor()
+
+cur.execute("SELECT id,state,country,ST_AsGeoJSON(boundary,4326)::JSONB from states")
+states_data = cur.fetchall()
+conn.close()
+
+states = []
+
+for row in states_data:
+    feature = {
+        "type": "Feature",
+        "properties": {"id": row[0], "state": row[1], "country": row[2]},
+        "geometry": row[3],
+    }
+    states.append(feature)
+
+geo = json.dumps({"type": "FeatureCollection", "features": states})
+
+try:
+    with open("./app/state.geojson", "w") as f:
+        f.write(geo)
+except Exception as e:
+    print(e)
+
+
