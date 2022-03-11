735ff12e23fda2d46c5294f784b4f3e72e08b7bc
Shalini <shalinisharma1297@gmail.com>
2022-03-11 22:50:07 +0530

    [GG-2487] set environment variable (MAPBOX_ACCESS_TOKEN)

--- script/state.py
+++ script/state.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from time import sleep
 
 import psycopg2
@@ -18,7 +19,7 @@ states_data = cur.fetchall()
 conn.close()
 
 if len(states_data)==0:
-    print("No data fetched from database ")
+    logging.ERROR("No data fetched from database ")
     quit()
 
 states = []
@@ -45,15 +46,17 @@ togeojsontiles.geojson_to_mbtiles(
     maxzoom=10
 )
 
-service= Uploader()
+service = Uploader()
 mapid = "state"
 
 with open('app/state.mbtiles', 'rb') as src:
     upload_resp = service.upload(src, mapid)
 
-    """ response status code 422 indicates that the server understands the content type of the request entity,
-     and the syntax of the request entity is correct, but it was unable to process the contained instructions. To overcome this problem 
-    script in sleep mode for 5 second and then retry"""
+    """ 
+    response status code 422 indicates that the server understands the content type of the request entity,
+    and the syntax of the request entity is correct, but it was unable to process the contained instructions. 
+    To overcome this problem script in sleep mode for 5 second and then retry.
+    """
 
 if upload_resp.status_code == 422:
     for request in range(5):
