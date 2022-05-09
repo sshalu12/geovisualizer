bafeaae7e572e73372b812895594a35011139a80
Shalini <shalinisharma1297@gmail.com>
2022-05-09 15:25:22 +0530

    [GG-2631] Implement Signup Api and added docstring in API

--- script/api.py
+++ script/api.py
@@ -19,45 +19,84 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
+@app.route('/signup', methods=['POST'])
+def signup():
+    """SIGNUP API
+    Parameters:
+    username (String): Username of user
+    email (String): Email of user
+    password (String): Password of user
+
+    Returns:
+        Http Response: {
+            String : Message 
+            Integer : Status Code
+        }
+    """
+
+    username=request.json.get('username')
+    email = request.json.get('email')
+    password = request.json.get('password')
+    cur.execute("SELECT email FROM users WHERE email='{}'".format(email))
+    user_with_email=cur.fetchone()
+
+    if user_with_email :
+        return jsonify({"Message": "User already exist"}),400
+        
+    cur.execute("INSERT INTO users (name,email,password) VALUES ('{}','{}',crypt('{}',gen_salt('bf')))".format(username,email,password))
+    conn.commit()
+    return jsonify({"Message": "User Registered Successfully"})
+        
+
 
 @app.route('/login', methods=['POST'])
 def login():
+    """LOGIN API
+    Parameters:
+    email (String): Email of user
+    password (String): Password of user
 
+    Returns:
+        Http Response: {
+            String : It can be either Message or username
+            Integer : Status Code
+        }
+    """
     email = request.json.get('email')
     password = request.json.get('password')
-    cur.execute("SELECT * FROM users WHERE email='{}' ".format(email))
-    user_with_email = cur.fetchone()
+    cur.execute("SELECT * FROM users WHERE email='{}' AND password=crypt('{}',password)".format(email, password))
+    user = cur.fetchone()
+
+    if not user:
+        return jsonify({"Message": "Either email or password is incorrect"}),401
     
-    if user_with_email :
-        cur.execute(
-            "SELECT * FROM users WHERE email='{}' AND password=crypt('{}',password)".format(email, password))
-        user = cur.fetchone()
-        if user:
-            id = user[0]
-            if 'user_id' in session: 
-                if session['user_id']==id:
-                    return jsonify({"username": user[1]})
-                else:
-                    session['user_id']=id
-                    return jsonify({"username": user[1]})
-            else:
-                session['user_id'] = id
-                return jsonify({"username": user[1]})
-        else:
-            return jsonify({"message": "Either email or password is incorrect"}),401
+    id,username=user[0],user[1]
 
-    else:
-        return jsonify({"message": "Either email or password is incorrect"}),401
+    if not 'user_id' in session: 
+        session['user_id'] = id
+        return jsonify({"Username": username})
+    
+    if not session['user_id']==id:
+        return jsonify({"Message":"Bad Request"}),400
 
-
+    return jsonify({"username": username})
+  
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    if 'user_id' in session:
-        session.pop('user_id', None)
-        return jsonify({"message": "log out successfully."})
-    else:
-        return jsonify({"message": "Bad request."}),400
+    """LOGOUT API
+
+    Returns:
+        Http Response: {
+            String : Message
+            Integer : Status Code
+        }
+    """
+    if not 'user_id' in session:
+        return jsonify({"Message": "Bad Request."}),400
+    
+    session.pop('user_id', None)
+    return jsonify({"Message": "Logged Out Successfully."})
 
 
 if __name__ == "__main__":
